d915fcdd18c88387f96c815f6d456ab9536bbe67
召伯 <quanyun.mqy@alibaba-inc.com>
2014-09-25 19:06:51 +0800

    dialog

--- app/js/index.js
+++ app/js/index.js
@@ -9,8 +9,9 @@ console.log("httpd start @8000");*/
 // console.log(global);
 var fs = require('fs'),
     // path = require('path'),
-	mongoose = require('mongoose');
-	_ = require('underscore');
+	mongoose = require('mongoose'),
+	_ = require('underscore'),
+	Segment = require('segment').Segment;
 
 mongoose.connect('mongodb://localhost/haimianbaobao');
 var db = mongoose.connection;
@@ -18,6 +19,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   console.log('mongodb连接成功');
 });
+//以下代码应该在数据库连接成功的回调中进行，暂时放在外面
 var _path = process.cwd() + '/app/js/db-schema';
 fs.readdirSync(_path).forEach(function (file) {
 	if (file.indexOf('.js')){
@@ -34,9 +36,15 @@ User.find(function(err, persons){
 // user.save();
 // console.log(user.name);
 
+// 创建分词实例
+var segment = new Segment();
+// 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
+segment.useDefault();
+
 //关闭启动画面
 setTimeout(function(){
 	$('.wecome-wrp').fadeOut();
+	$('.study-wrp').show();
 }, 1800);
 
 var groupTmpl = '<div class="study-group form-horizontal">'+
@@ -58,21 +66,61 @@ $('body').on('click', '.add-study-group', function(){
 	$(this).before(groupTmpl);
 });
 
-//保存输入的条目
+//保存输入的条目，并进入对话模式
 $('body').on('click', '.save-study', function(){
-	var Knowledge = mongoose.model('Knowledge');
+	var KnowledgeModel = mongoose.model('Knowledge');
 	$('.study-group').each(function(k, v){
 		var _qItem = $(v).find('.study-question'),
 			_q = _qItem.val(),
 			_aItem = $(v).find('.study-answer'),
 			_a = _aItem.val();
 		if(!!_q){
-			var knowledge = new Knowledge({'question': _q, 'answer': _a});
-			knowledge.save();
+			_q = '^' + _.map(segment.doSegment(_q), function(v, k){
+				return v.w;
+			}).join('^');
+			//先去查数据库中是否有相同的问题，如果有，则更新答案；如果没有，就写一条新记录
+			KnowledgeModel.find({'question': _q}, function(err, q){
+				if(q.length){
+					KnowledgeModel.update({'_id': q[0]._id}, {$set:{'answer': _a}, function(err, ques){
+						console.log(err);
+					}});
+				}else{
+					var knowledge = new KnowledgeModel({'question': _q, 'answer': _a});
+					knowledge.save();
+				}
+			});
 			_qItem.val('');
 			_aItem.val('');
+			$('.study-wrp').fadeOut();
+			$('.chat-wrp').show();
 		}
 	});
 });
+//点击开始玩耍
+$('body').on('click', '.play-game', function(){
+	$('.study-wrp').fadeOut();
+	$('.chat-wrp').show();
+});
 
-
+//发消息
+$('body').on('click', '#send-msg', function(){
+	var msg = $('#msg-box').val();
+	if(msg){
+		//把消息放入到对话窗口中
+		$('.chat-items').append('<li class="chat-item me-sent"><span class="bubble">' + msg + '</span></li>');
+		$('#msg-box').val('');
+		$('.chat-items-wrp').scrollTop($('.chat-items').height() - $('.chat-items-wrp').height());
+		var KnowledgeModel = mongoose.model('Knowledge');
+		KnowledgeModel.find({'question': msg}, function(err, answer){
+			$('.chat-items').append('<li class="chat-item"><span class="bubble">' + (answer.length ? answer[0].answer : '对不起，您还没有告诉我应该怎么回答') + '</span></li>');
+			$('.chat-items-wrp').scrollTop($('.chat-items').height() - $('.chat-items-wrp').height());
+		});
+	}
+});
+//定义发消息的键盘事件
+$('#msg-box').keydown(function(ev){
+	if(ev.keyCode === 13){//enter // && ev.ctrlKey
+		ev.preventDefault();
+		$('#send-msg').trigger('click');
+	}
+});
